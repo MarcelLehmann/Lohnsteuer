14624116728be17d44b1c164e9a2bfcacae919d3
Marcel Lehmann <develop@marcellehmann.de>
2015-01-08 23:21:52 +0100

    # Test implementiert (Verwendung des WebServices vom BMF)

--- LohnPapGenerator/src/de/powerproject/lohnpap/TestAufruf.java
+++ LohnPapGenerator/src/de/powerproject/lohnpap/TestAufruf.java
@@ -1,10 +1,13 @@
 package de.powerproject.lohnpap;
 
+import java.io.File;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,13 +18,44 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+/**
+ * 
+ * Test des generierten Lohnsteuerrechners gegen die externe
+ * Programmierschnittstelle des Bundesministerium für Finanzen (BMF)
+ * 
+ * Infos unter https://www.bmf-steuerrechner.de/interface/
+ * 
+ * @author Marcel Lehmann
+ * 
+ */
+
 public class TestAufruf extends TestCase {
+
+	File tmp;
+
+	@Override
+	protected void setUp() throws Exception {
+		tmp = File.createTempFile("bmf.test.", ".xml");
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		tmp.delete();
+	}
 
 	public void testLohnsteuer() throws Exception {
 
-		for (int i = 5000; i <= 80000; i += 2500) {
+		for (int lohn = 5000; lohn <= 80000; lohn += 2500) {
 
-			check(1, new BigDecimal(i * 100), 1);
+			System.out.print("Jahresarbeitslohn " + lohn + " ");
+
+			BigDecimal re4 = new BigDecimal(lohn * 100); // Angabe in Cent
+
+			for (int stkl = 1; stkl <= 6; stkl++) {
+				check(1, re4, stkl);
+			}
+
+			System.out.println(" ok");
 
 		}
 	}
@@ -41,23 +75,32 @@ public class TestAufruf extends TestCase {
 	private void check(int lzz, BigDecimal re4, int stkl) throws Exception {
 
 		URL url = new URL(
-				"http://www.bmf-steuerrechner.de/interface/2015.jsp?LZZ=" + lzz
-						+ "&RE4=" + re4 + "&STKL=" + stkl);
+				"https://www.bmf-steuerrechner.de/interface/2015.jsp?LZZ="
+						+ lzz + "&RE4=" + re4.intValue() + "&STKL=" + stkl);
+
+		System.out.print(stkl);
+
 		URLConnection con = url.openConnection();
 		InputStream is = con.getInputStream();
+
+		Files.copy(is, tmp.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
 		DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
 		f.setNamespaceAware(false);
 		f.setIgnoringElementContentWhitespace(false);
 		DocumentBuilder b = f.newDocumentBuilder();
-		Document d = b.parse(is);
+		Document d = b.parse(tmp);
 
 		Lohnsteuer2015Big ls = new Lohnsteuer2015Big();
 		ls.LZZ = lzz;
 		ls.RE4 = re4;
 		ls.STKL = stkl;
+		ls.main();
 
 		NodeList ausgaben = d.getElementsByTagName("ausgabe");
+
+		assertNotNull(ausgaben);
+		assertTrue(ausgaben.getLength() > 0);
 
 		for (int i = 0; i < ausgaben.getLength(); i++) {
 			Element n = (Element) ausgaben.item(i);
@@ -69,7 +112,7 @@ public class TestAufruf extends TestCase {
 			if (o != null) {
 				o = o.toString();
 			}
-			assertEquals(value, o);
+			assertEquals("Wert " + name, value, o);
 
 		}
 	}
